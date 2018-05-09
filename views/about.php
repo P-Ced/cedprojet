d20e58de45d9bc75824729233c00b2ce785c4aeb
CED <35234096+P-Ced@users.noreply.github.com>
2018-05-10 00:12:18 +0200

    Delete about.php

--- views/about.php
+++
@@ -1,8 +0,0 @@
-<?php ob_start() ?>
-Voici mon projet.<br>
-Il permet de s'authenitifer et de gérer des utilisateurs.
-<?php
-$title = 'A propos';
-$content = ob_get_clean();
-include 'includes/layout.php';
-?>
